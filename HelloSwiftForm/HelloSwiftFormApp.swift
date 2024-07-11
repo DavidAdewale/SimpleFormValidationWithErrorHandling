fbf1ed4d9e2f2e87e0784f60fd10555f63384e8f
DavidAdewale <adewaledavidola@gmail.com>
2024-07-11 14:30:03 +0100

    Initial Commit

---
+++ HelloSwiftForm/HelloSwiftFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftFormApp.swift
+//  HelloSwiftForm
+//
+//  Created by David Adewale on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
