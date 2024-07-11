d55733bca914deee1eb3c8c64341ea2369693bb3
DavidAdewale <adewaledavidola@gmail.com>
2024-07-11 17:41:04 +0100

    simple form validation with basic error handling

---
+++ HelloSwiftForm/Form Errors/LoginFormError.swift
@@ -0,0 +1,13 @@
+//
+//  LoginFormError.swift
+//  HelloSwiftForm
+//
+//  Created by David Adewale on 11/07/2024.
+//
+
+import Foundation
+
+struct LoginFormError {
+    var email: String = ""
+    var password: String = ""
+}
